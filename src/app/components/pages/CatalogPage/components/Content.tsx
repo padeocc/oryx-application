ffc3a888c9b35017069a31a73b2620af49feb909
Benjamin Talou <benjamin.talou@kaduceo.com>
2024-03-26 10:58:41 +0100

    WIP

--- src/app/components/pages/CatalogPage/components/Content.tsx
+++ src/app/components/pages/CatalogPage/components/Content.tsx
@@ -43,7 +43,7 @@ const Content = ({
       <Grid>
         <GridCol span={{ base: 12, sm: 8 }}>
           <Title order={2}>Quelques inspirations pour vous aujourd&apos;hui !</Title>
-          <Title order={3}>{loading ? '' : `${data.length} services trouvés`}</Title>
+          <Title order={3}>{loading ? '' : `${data.length} actions trouvées`}</Title>
         </GridCol>
         <GridCol span={{ base: 12, sm: 4 }} ta={'right'}>
           <Group align="flex-end" justify="flex-end">
