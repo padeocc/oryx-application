9015d9e54be8375ac1383fb27308b5f76be68ad0
MEHMETTOKGOZ <42mt06@gmail.com>
2025-09-30 10:42:06 +0200

    l'ajout adresse exact rebase a été completé

--- src/components/pages/ServiceAdditionPage/components/Form.tsx
+++ src/components/pages/ServiceAdditionPage/components/Form.tsx
@@ -23,6 +23,7 @@ import { redirect } from 'next/navigation';
 import { RefObject, useRef, useState } from 'react';
 import ReCAPTCHA from 'react-google-recaptcha';
 import { getOptionsFromThemes, getTagsFromThemes } from '../utils';
+import { Placeholder } from '@phosphor-icons/react';
 
 const urlRegexString =
   '^(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
@@ -93,7 +94,6 @@ const Form = ({
     { value: 'ngo', label: t('form-structure-ngo') },
     { value: 'other', label: t('form-structure-other') }
   ];
-
 
   return (
     <>
