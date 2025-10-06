a5681f87520005e08cbf1d66514501f3be40566d
GuillaumeCasbas <guillaume.casbas@gmail.com>
2025-10-06 14:27:43 +0200

    feat: add cta to landing pages on the home page

--- src/components/pages/HomePage/components/ThemeSection.tsx
+++ src/components/pages/HomePage/components/ThemeSection.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { Theme, themesColors, themesIcons } from '@/config';
+import { landingPagesUrl, Theme, themesColors, themesIcons } from '@/config';
 import { Service } from '@/types';
-import { Grid, GridCol, Group, Stack, Text, Title } from '@mantine/core';
+import { Anchor, Button, Grid, GridCol, Group, Stack, Text, Title } from '@mantine/core';
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 import { useState } from 'react';
@@ -22,26 +22,38 @@ const ThemeSection = ({ items, theme }: { items: Service[]; theme: Theme }) => {
     href: `/services?filters={"theme":["${theme}"]}`,
     style: { cursor: 'pointer', color: 'inherit', textDecoration: 'none' }
   };
+console.log(landingPagesUrl.get('events'));
 
   return (
     <Stack onMouseOver={() => setMouseOver(true)} onMouseLeave={() => setMouseOver(false)}>
-      <Group gap={'xs'}>
-        <Title order={3}>
-          <Group justify="center" align="center" c={color} gap={'xs'}>
-            <Icon size="1.4rem" />
-            <Text fz={'xl'} c={'inherit'} fw="bold">
-              {tTheme(theme)}
+      <Group w="100%" grow preventGrowOverflow={false}>
+        <Group gap={'xs'}>
+          <Title order={3}>
+            <Group justify="center" align="center" c={color} gap={'xs'}>
+              <Icon size="1.4rem" />
+              <Text fz={'xl'} c={'inherit'} fw="bold">
+                {tTheme(theme)}
+              </Text>
+            </Group>
+          </Title>
+          {mouseOver ? (
+            <Text visibleFrom="sm" {...moreLinkProps} className={style['more-text-container']}>
+              {tCommon('see_more_theme', { theme: tTheme(theme) })} {'>'}
             </Text>
-          </Group>
-        </Title>
-        {mouseOver ? (
-          <Text visibleFrom="sm" {...moreLinkProps} className={style['more-text-container']}>
+          ) : null}
+          <Text hiddenFrom="sm" {...moreLinkProps}>
             {tCommon('see_more_theme', { theme: tTheme(theme) })} {'>'}
           </Text>
-        ) : null}
-        <Text hiddenFrom="sm" {...moreLinkProps}>
-          {tCommon('see_more_theme', { theme: tTheme(theme) })} {'>'}
-        </Text>
+        </Group>
+        <Group justify="end" align="right">
+          {landingPagesUrl.get(theme) ? (
+            <Button variant="filled">
+                <Anchor c="white" href={landingPagesUrl.get(theme)}>
+                    {tCommon('read_more')}
+                </Anchor>
+            </Button>
+          ) : null}
+        </Group>
       </Group>
       <Grid justify="stretch" c={color}>
         {items.map((service, index) => (
