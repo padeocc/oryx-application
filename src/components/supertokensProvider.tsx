ed6bc68bb255cf88bfe471de7eb9bd8a778e51e6
Lupiac <antoine.lupiac0@gmail.com>
2025-11-06 17:38:37 +0100

    feat(favorite): fetch user from database when session is active to update CurrentUser context

--- src/components/supertokensProvider.tsx
+++ src/components/supertokensProvider.tsx
@@ -1,17 +1,43 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SuperTokensWrapper } from 'supertokens-auth-react';
 import SuperTokensReact from 'supertokens-auth-react';
 import { frontendConfig, setRouter } from '../app/config/frontend';
 import { usePathname, useRouter } from 'next/navigation';
 import { EmailPasswordComponentsOverrideProvider } from 'supertokens-auth-react/recipe/emailpassword';
+import { useCurrentUserDispatch } from '@/app/context/CurrentUserContext';
+import { FetchUserResponse, User } from '@/types';
 
+const getCurrentUser = async () => {
+  const baseURL = process?.env?.NEXT_PUBLIC_AUTH_APPINFO_WEBSITEDOMAIN || '';
+  const url = `${baseURL}/api/user`;
+  const currentUser: User = await fetch(url, {
+    method: 'GET'
+  })
+    .then(res => {
+      return res.json();
+    })
+    .then((userResponse: FetchUserResponse) => {
+      return userResponse.user;
+    });
+  return currentUser;
+};
+
+let currentUser: User;
 if (typeof window !== 'undefined') {
   // we only want to call this init function on the frontend, so we check typeof window !== 'undefined'
   SuperTokensReact.init(frontendConfig());
+  currentUser = await getCurrentUser();
 }
 
 export const SuperTokensProvider: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
+  const dispatch = useCurrentUserDispatch();
+  useEffect(() => {
+    const asyncFunc = async (dispatch: any) => {
+      dispatch({ type: 'updateUser', user: currentUser });
+    };
+    asyncFunc(dispatch);
+  }, [currentUser]);
   setRouter(useRouter(), usePathname() || window.location.pathname);
 
   return (
@@ -33,7 +59,7 @@ export const SuperTokensProvider: React.FC<React.PropsWithChildren<{}>> = ({ chi
             );
           }
         }}>
-      {children}
+        {children}
       </EmailPasswordComponentsOverrideProvider>
     </SuperTokensWrapper>
   );
